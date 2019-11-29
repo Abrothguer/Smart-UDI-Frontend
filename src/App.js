28143f4d4f2dd9935a6602c5af23a9aebb194e07
Arthur Borges Duarte <arthur@governa.com.br>
2019-11-29 17:54:50 -0300

    Map integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { DatePicker, Layout } from 'antd';
+import { Layout } from 'antd';
+// import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import 'antd/dist/antd.css';
+
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={15}
+    defaultCenter={{ lat: -18.9128, lng: -48.2755 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -18.9128, lng: -48.2755 }} />}
+  </GoogleMap>
+))
+
 
 class App extends React.Component {
 
@@ -9,16 +22,24 @@ class App extends React.Component {
 
     return (
       <Layout className="layout">
-        <Header>
-
+        <Header style={{ background: '#1e8e3e'}}>
+          <h1 style={{ color: 'white', fontSize: '30px' }}>Ocellot</h1>
         </Header>
-        <Content>
-          <h1>Helloson</h1>
-          <DatePicker />
+        <Content style={{ paddingTop: '0px' }}>
+
+          <MyMapComponent
+
+            isMarkerShown
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `90vh` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+
         </Content>
 
         <Footer style={{ textAlign: 'center' }}>
-          Smart UDI Flood Predictor &copy; 2019<br/> Feito com <span role="img" aria-label="heart">🖤</span> por Ocellot.
+          Smart UDI Flood Predictor &copy; 2019<br/> Feito com <span role="img" aria-label="heart">💚</span> por Ocellot <span role="img" aria-label="heart">🐈</span>
         </Footer>
       </Layout>
     )
